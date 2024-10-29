170b2176e36f9df0e27a6653153b42deb4dd9a47
max.sch <schaetz.maximilian@gmail.com>
2024-10-29 09:38:34 +0100

    first version

--- src/ue02_2.java
+++ src/ue02_2.java
@@ -2,6 +2,37 @@ import libraries.Out;
 
 public class ue02_2 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+
+        int year = scanner.nextInt();
+        int month = scanner.nextInt();
+
+        boolean isLeapYear = false;
+
+        if (year %4 == 0 && year %400 == 0) {
+            
+            isLeapYear = true;
+            
+        }
+        else{
+            isLeapYear = false;
+        }
+
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                Out.println("31 Tage");
+                break;
+            case 4: case 6: case 9: case 11:
+                Out.println("30 Tage");
+                break;
+
+            case 2:
+                if(isLeapYear){
+                    Out.println("29 Tage");
+                }
+                else{
+                    Out.println("28 Tage");
+                }
+
+        }
     }
 }
