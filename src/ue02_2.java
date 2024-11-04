95294167b55e4bc226d13b68c48c2558c36a51f5
max.sch <schaetz.maximilian@gmail.com>
2024-11-04 10:45:02 +0100

    First commit

--- src/ue02_2.java
+++ src/ue02_2.java
@@ -1,14 +1,33 @@
+import java.util.Scanner;
 import libraries.Out;
 
 public class ue02_2 {
     public static void main(String[] args) {
 
-        int year = scanner.nextInt();
-        int month = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+
+        int month;
+        int year;
+        
+        do {
+            Out.print("Enter a positive year: ");
+            year = scanner.nextInt();
+            if (year <= 0) {
+                System.out.println("Year must be a positive integer. Please try again.");
+            }
+        } while (year <= 0);
+
+        do {
+            Out.print("Enter a month (1-12): ");
+            month = scanner.nextInt();
+            if (month < 1 || month > 12) {
+                System.out.println("Month must be between 1 and 12. Please try again.");
+            }
+        } while (month < 1 || month > 12);
 
         boolean isLeapYear = false;
 
-        if (year %4 == 0 && year %400 == 0) {
+        if ((year %4 == 0 && year %100 != 0) || (year %400 == 0)) {
             
             isLeapYear = true;
             
@@ -32,7 +51,10 @@ public class ue02_2 {
                 else{
                     Out.println("28 Tage");
                 }
-
+                break;
+            default:
+                System.out.println("Invalid month entered. Please enter a month between 1 and 12.");
         }
+          scanner.close();
     }
 }
