379a8f585e64eb5c058b607293af93d637dba23d
max.sch <schaetz.maximilian@gmail.com>
2024-11-04 11:03:33 +0100

    first commit ue02_3

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -1,7 +1,30 @@
-import libraries.Out;
+import java.util.Scanner;
 
 public class ue02_3 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        double x = scanner.nextDouble();
+
+        double y = scanner.nextDouble();
+        
+
+        if( x > 0 && y > 0)
+        {
+            if(Math.sqrt((x*x) + (y*y)) > 4) {
+                System.out.println("Der Punkt ("+ x +"|"+ y + ") ist außerhalb des Kreises!");
+
+            }
+            else {
+                System.out.println("Der Punkt ("+ x +"|"+ y + ") ist innerhalb des Kreises!");
+            }
+        }
+        else{
+            System.out.println("Der Punkt ("+ x +"|"+ y + ") ist nicht im 1. Quadranten!");
+        }
+
+
+        scanner.close();
     }
 }
