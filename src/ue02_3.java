80f2f5bfeef4d930a965725a206639e1b0358cfb
max.sch <schaetz.maximilian@gmail.com>
2024-11-05 16:32:34 +0100

    2. Version

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -4,27 +4,25 @@ public class ue02_3 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("X-Koordinate:");
         double x = scanner.nextDouble();
-
+        System.out.println("Y-Koordinate:");
         double y = scanner.nextDouble();
-        
 
-        if( x >= 0 && y >= 0)
+
+        if(x >= 0 && y >= 0 && x <= 4 && y <= 4)
         {
             if(Math.sqrt((x*x) + (y*y)) > 4) {
-                System.out.println("Der Punkt ("+ x +"|"+ y + ") ist außerhalb des Kreises!");
-
+                System.out.println("Der Punkt ("+ x +"|"+ y + ") ist innerhalb der grünen Fläche!");
             }
             else {
-                System.out.println("Der Punkt ("+ x +"|"+ y + ") ist innerhalb des Kreises!");
+                System.out.println("Der Punkt liegt im Kreis!");
             }
         }
         else{
-            System.out.println("Der Punkt ("+ x +"|"+ y + ") ist nicht im 1. Quadranten!");
+            System.out.println("Der Punkt liegt außerhalb des Kreises und der grünen Fläche!");
         }
-
-
+        
         scanner.close();
     }
 }
